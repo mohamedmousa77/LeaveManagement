eb0488cd6639a948a5f6f8aa32217023c74b7f1e
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2026-03-13 02:51:20 +0100

    feat(init) Creating the basic project instructure

---
+++ HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace HR.LeaveManagement.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+}
