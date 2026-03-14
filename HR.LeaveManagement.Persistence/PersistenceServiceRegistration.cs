a3d4eb7d364a589c07fca25f0facbe490a35701b
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2026-03-14 02:03:33 +0100

    feat(infrasturcture): Implement Repositories and register them in DI file 'Persistence Service Registration'

--- HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using HR.LeaveManagement.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +15,12 @@ public static class PersistenceServiceRegistration
         {
             options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
         });
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+        services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+        services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
 
         return services;
     }
