e31a6fd28ab786c3a5a804f0711d6797b0d14b9c
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2026-04-07 10:24:29 +0200

    Implement Unit Testing

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypesRepository.cs
@@ -0,0 +1,47 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System.Collections.Generic;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks;
+
+public class MockLeaveTypesRepository
+{
+    public static Mock<ILeaveTypeRepository> GetMockLeaveTypesRepository()
+    {
+        var leaveTypes = new List<LeaveType>
+        {
+            new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 10,
+                Name = "Test Vacation"
+            },
+            new LeaveType
+            {
+                Id = 2,
+                DefaultDays = 15,
+                Name = "Test Sick"
+            },
+            new LeaveType
+            {
+                Id = 3,
+                DefaultDays = 15,
+                Name = "Test Maternity"
+            }
+        };
+
+        var mockRepo = new Mock<ILeaveTypeRepository>();
+
+        mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+        mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+        return mockRepo;
+    }
+}
